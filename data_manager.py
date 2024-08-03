b2f9ffb8137365d828907b98bccd5cc9399f4548
githubkg8 <kosztolanyi.gabor95@gmail.com>
2024-08-03 15:26:09 +0200

    new file: sql_manager

--- data_manager.py
+++ data_manager.py
@@ -8,7 +8,8 @@ from datetime import datetime, timedelta
 from credentials import ENTSOE_TOKEN, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DATABASE
 from entsoe_codes import EntsoeCodes
 from timezones import TimeZoneManager
-from sqlalchemy import create_engine
+from sql_manager import SQLManager
+
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -19,7 +20,7 @@ class DataManager():
     def __init__(self,local_timezone) -> None:
         self.entsoe_codes=EntsoeCodes()
         self.timezone_manager=TimeZoneManager(local_timezone)
-        self.db_engine = create_engine(url = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DATABASE}')
+        self.sql_manager=SQLManager()
         self.base_url=f"https://web-api.tp.entsoe.eu/api?securityToken={ENTSOE_TOKEN}"
 
     def get_entsoe_response(self,params):
@@ -33,15 +34,6 @@ class DataManager():
             soup = BeautifulSoup(response.text, 'xml')
             logger.error(f"ENTSO-E API CALL Error: {response.status_code}, {soup.find('Reason').find('text').text}")
         return response
-
-    def upload_sql(self,df,table_name,schema):
-        '''Uploads a pandas dataframe to a SQL table'''
-
-        try:
-            df.to_sql(table_name, con=self.db_engine, schema=schema, if_exists='append', index=False)
-            logger.info(f"{table_name} uploaded successfully!")
-        except Exception as e:
-            logger.error(f"Error while uploading {table_name}: {e}")
 
     def get_power_prices(self,periodStart,periodEnd):
         '''Get the day ahead power prices for Hungary, UTC timezone, fromat: YYYYMMDDhhmm'''
@@ -76,7 +68,10 @@ class DataManager():
         # Get the last timestamp from the database 
         # Set the periodStart and periodEnd for day ahead
         # Convert the local timezone to UTC
-        last_timestamp=pd.read_sql('SELECT MAX("UTC") FROM "HUN".power_price', con=self.db_engine).values[0][0]
+        table_name='power_price'
+        schema_name='HUN'
+
+        last_timestamp=self.sql_manager.read_sql(table_name,schema_name)
         day_ahead_end = datetime.now() + timedelta(days=2)
         periodStart_localtz = datetime(last_timestamp.year,last_timestamp.month,last_timestamp.day,0,0) + timedelta(days=1)
         periodEnd_localtz = datetime(day_ahead_end.year,day_ahead_end.month,day_ahead_end.day,0,0)
@@ -92,9 +87,10 @@ class DataManager():
             try:
                 table_name='power_price'
                 schema_name='HUN'
-                self.upload_sql(df_da_prices,table_name,schema_name)
-                logger.info(f"power_prices refreshed successfully! ({periodStart_localtz.strftime('%Y-%m-%d')} - {(periodEnd_localtz+timedelta(-1)).strftime('%Y-%m-%d')})")
-                return "Success"
+                success=self.sql_manager.upload_sql(df_da_prices,table_name,schema_name)
+                if success:
+                    logger.info(f"power_prices refreshed successfully! ({periodStart_localtz.strftime('%Y-%m-%d')} - {(periodEnd_localtz+timedelta(-1)).strftime('%Y-%m-%d')})")
+                    return "Success"
             except Exception as e: 
                 logger.error(f"Error while refreshing power_prices: {e}")
                 return f"Error: {e}"
---
+++ sql_manager.py
@@ -0,0 +1,36 @@
+import logging
+from sqlalchemy import create_engine
+import pandas as pd
+
+from credentials import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DATABASE
+
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+class SQLManager():
+    def __init__(self) -> None:
+        self.db_engine = create_engine(url = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@localhost:5432/{POSTGRES_DATABASE}')
+
+    def upload_sql(self,df,table_name,schema):
+        '''Uploads a pandas dataframe to a SQL table'''
+
+        try:
+            df.to_sql(table_name, con=self.db_engine, schema=schema, if_exists='append', index=False)
+            return True
+        except Exception as e:
+            logger.error(f"Error while uploading {table_name}: {e}")
+
+    def read_sql(self,table_name,schema_name):
+        '''Reads a SQL table to a pandas dataframe'''
+
+        try:
+            last_timestamp=pd.read_sql(f'SELECT MAX("UTC") FROM "{schema_name}".{table_name}', con=self.db_engine).values[0][0]
+            logger.info(f"{table_name} table read successfully!")
+        except Exception as e:
+            logger.error(f"Error while reading {table_name}: {e}")
+        return last_timestamp
+    
+
+        
